7668be2a9e1663c7e87bf99501a92096c2bd5a26
aearavindh <42218567+aearavindh@users.noreply.github.com>
2019-12-02 17:29:49 +0530

    Update NewTest.java

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -7,21 +7,19 @@ import org.testng.annotations.Test;
 public class NewTest {
   @Test
   public void test() throws InterruptedException {
-	  System.setProperty("webdriver.chrome.driver",
-		        "D:\\Eclipse\\test99\\chromedriver.exe");
-		ChromeOptions opt = new ChromeOptions();
-		opt.addArguments("--headless");
-		opt.addArguments("window-size=1920x1080");
-		opt.addArguments("--no-sandbox");
-		WebDriver driver = new ChromeDriver();
-		driver.get("http://3.134.101.222:8080/BMI/");
-        Thread.sleep(1000);
-		driver.findElement(By.name("weight")).sendKeys("80.0"); 
-		Thread.sleep(1000);
-		driver.findElement(By.name("height")).sendKeys("2.0"); 
-		Thread.sleep(1000);
-		driver.findElement(By.cssSelector("input:nth-child(8)")).click();
-		Thread.sleep(2000);
+       System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+       ChromeOptions opt = new ChromeOptions();
+       opt.addArguments("--headless");
+       opt.addArguments("window-size=1920x1080");
+       WebDriver driver = new ChromeDriver();
+       driver.get("http://3.134.101.222:8080/BMI/");
+       Thread.sleep(1000);
+       driver.findElement(By.name("weight")).sendKeys("80.0"); 
+       Thread.sleep(1000);
+       driver.findElement(By.name("height")).sendKeys("2.0"); 
+       Thread.sleep(1000);
+       driver.findElement(By.cssSelector("input:nth-child(8)")).click();
+       Thread.sleep(2000);
       String h= driver.getTitle();
       System.out.println(h);
       String e= "BMI Calculator";
